a23bf3787fa793a2daf1ea8d36955a6ba4820427
Michael <mfinch@andrew.cmu.edu>
2021-03-11 12:59:25 -0500

    update

--- main_app.py
+++ main_app.py
@@ -3,6 +3,7 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+from vega_datasets import data
 from helper_func import*
 
 #
@@ -15,4 +16,16 @@ df = load_data(Data_folder)
 #
 #
 
-st.write(df[1])
+st.write(df[0])
+
+#US State Background
+states = alt.topo_feature(data.us_10m.url, feature ='state')
+Background = alt.Chart(states).mark_geoshape(
+    fill = 'lightgray',
+    stroke = 'black'
+).properties(
+    width=500,
+    height=300
+).project('albersUsa')
+
+Background

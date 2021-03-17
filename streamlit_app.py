b086f660e81548a5b84c36a7234bb934c12ba503
mfinch-github <mfinch2009@my.fit.edu>
2021-03-16 22:10:20 -0400

    map added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+from vega_datasets import data
 
 st.title("Let's look at some social mobility data 📊.")
 st.write("More particularly, let's see how income affects the life expectancy of Americans at a state level")
@@ -15,7 +16,6 @@ def load_data(data_file, file_type):
 df1 = load_data("health_ineq_online_table_3.csv", "csv")
 df2 = load_data("health_ineq_online_table_4.csv", "csv")
 df3 = load_data("health_ineq_online_table_5.csv", "csv")
-# df4 = load_data("health_ineq_online_table_3_perc.csv", "csv")
 
 st.write("Here's the raw data we would be examining")
 
@@ -23,19 +23,9 @@ agg_data = df1[["statename", "le_agg_q1_F", "le_agg_q2_F", "le_agg_q3_F", "le_ag
 agg_data["le_agg_F"] = agg_data[["le_agg_q1_F", "le_agg_q2_F", "le_agg_q3_F", "le_agg_q4_F"]].mean(axis=1)
 agg_data["le_agg_M"] = agg_data[["le_agg_q1_M", "le_agg_q2_M", "le_agg_q3_M", "le_agg_q4_M"]].mean(axis=1)
 
-# st.write(df1)
-# st.write(df2)
+
 st.write(agg_data)
 
-# st.write("Hmm 🤔, is there some correlation between body mass and flipper length? Let's make a scatterplot with [Altair](https://altair-viz.github.io/) to find.")
-
-# chart = alt.Chart(agg_data).mark_point().encode(
-#     x=alt.X("statename", scale=alt.Scale(zero=False)),
-#     y=alt.Y("le_agg_F", scale=alt.Scale(zero=False)),
-#     color=alt.Y("statename")
-# ).properties(
-#     width=600, height=400
-# ).interactive()
 
 highlight = alt.selection(type='single', on='mouseover',
                           fields=['symbol'], nearest=True)
@@ -82,3 +72,144 @@ chart = alt.Chart(data).mark_line().encode(
 )
 
 st.write(chart)
+
+
+###
+#
+###
+
+## use airport data for lat and long of map background
+airports = data.airports()
+airports = airports[["state","latitude","longitude"]]
+state_loc = airports.groupby(['state']).mean()
+
+source = df3
+source["state"] = source["stateabbrv"]
+source = source.set_index('state').join(state_loc)
+
+source["index"] =  list(range(len(source))) 
+source = source.set_index('index')
+
+## data that will be considered
+source["state"] = source["statename"]
+source["Female, Income: Q1"] = source["le_agg_q1_F"]
+source["Female, Income: Q2"] = source["le_agg_q2_F"]
+source["Female, Income: Q3"] = source["le_agg_q3_F"]
+source["Female, Income: Q4"] = source["le_agg_q4_F"]
+
+#US State Background # https://altair-viz.github.io/gallery/airports_count.html
+states = alt.topo_feature(data.us_10m.url, feature ='states')
+Background = alt.Chart(states).mark_geoshape(
+    fill = 'lightgray',
+    stroke = 'black'
+).properties(
+    width=500,
+    height=300
+).project('albersUsa')
+
+# year slider for Maps
+slider = alt.binding_range(min = source['year'].min(), max = source['year'].max(), step =1)
+select_year = alt.selection_single(name="year", fields=['year'], bind = slider, init={'year':source['year'].min()})
+
+base = alt.Chart(source)
+
+### trend line plot 
+
+plot=base.mark_point(filled=True   
+).encode(
+    alt.X('year:O', scale = alt.Scale(zero=False) ),
+    alt.Y('average(Female, Income: Q1):Q', scale = alt.Scale(zero=False)),
+    color=alt.value("salmon")
+)
+
+plot+=base.mark_point(filled =True   
+).encode(
+    alt.X('year:O', scale = alt.Scale(zero=False) ),
+    alt.Y('average(Female, Income: Q2):Q', scale = alt.Scale(zero=False)),
+    color=alt.value("salmon"),
+    shape = alt.value("cross")
+)
+
+plot+=base.mark_point(filled =True  
+).encode(
+    alt.X('year:O', scale = alt.Scale(zero=False) ),
+    alt.Y('average(Female, Income: Q3):Q', scale = alt.Scale(zero=False)),
+    color=alt.value("salmon"),
+    shape = alt.value("square")
+)
+
+plot+=base.mark_point(filled=True 
+).encode(
+    alt.X('year:O', scale = alt.Scale(zero=False) ),
+    alt.Y('average(Female, Income: Q4):Q', scale = alt.Scale(zero=False), axis=alt.Axis(title='Average Female Age')),
+    
+    color=alt.value("salmon"),
+    shape = alt.value("diamond"),
+    legend=alt.value(legend=alt.Legend)
+
+)
+
+
+### map plots for the 4 Q
+
+
+points_Female1 = base.mark_circle().encode(
+    longitude = "longitude:Q",
+    latitude = "latitude:Q",
+
+    size = alt.Size("Female, Income: Q1:Q", scale=alt.Scale(zero=False), legend=None),
+    color = alt.value("salmon"),
+    tooltip = ["state:N","Female, Income: Q1:Q"]
+  ).add_selection(
+      select_year,
+      
+  ).transform_filter(
+      select_year   
+  ).properties(title='Q1')
+
+points_Female2 = base.mark_circle().encode(
+    longitude = "longitude:Q",
+    latitude = "latitude:Q",
+
+    size = alt.Size("Female, Income: Q2:Q",scale=alt.Scale(zero=False), legend=None),
+    color = alt.value("salmon"),
+    tooltip = ["state:N","Female, Income: Q2:Q"]
+  ).add_selection(
+      select_year
+      
+  ).transform_filter(
+      select_year   
+  ).properties(title='Q2')
+
+points_Female3 = base.mark_circle().encode(
+    longitude = "longitude:Q",
+    latitude = "latitude:Q",
+
+    size = alt.Size("Female, Income: Q3:Q",scale=alt.Scale(zero=False), legend=None),
+    color = alt.value("salmon"),
+    tooltip = ["state:N", "Female, Income: Q3:Q"]
+  ).add_selection(
+      select_year
+      
+  ).transform_filter(
+      select_year   
+  ).properties(title='Q3')
+
+points_Female4 = base.mark_circle().encode(
+    longitude = "longitude:Q",
+    latitude = "latitude:Q",
+
+    size = alt.Size("Female, Income: Q4:Q",scale=alt.Scale(zero=False), legend=None),
+    color = alt.value("salmon"),
+    tooltip = ["state:N","Female, Income: Q4:Q"]
+  ).properties(title = '').add_selection(
+      select_year
+      
+  ).transform_filter(
+      select_year   
+  ).properties(title='Q4')
+
+Maps = (points_Female1 | points_Female2) & (points_Female3|points_Female4)
+
+st.write(Maps)
+st.write(plot)

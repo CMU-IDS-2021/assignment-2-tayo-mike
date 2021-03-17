b50adf2a0d1f089cbc866a692ddf32693560c9f7
root <tayojabar@gmail.com>
2021-03-17 04:58:36 +0200

    further updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,16 @@ import altair as alt
 st.title("Let's look at some social mobility data 📊.")
 st.write("More particularly, let's see how income affects the life expectancy of Americans at a state level")
 
+st.subheader("These are our goals: ")
+st.write("1. We set out to investigate the male life expectancy in all states +\n"
+        "2. We set out to investigate the female life expectancy in all states +\n"
+        "3. We set out to investigate the life expectancy as a time series between 2001 and 2014 across all states in the United States")
+
+st.subheader("Here are our findings: ")
+st.write("We came to find that generally, across the United States, the life expectancy is higher for women. We also found that the life expectancy is also highest for the population earning in the 4th percentile of their income bracket. I think this is a very logical trend because as with the typical world bank indicators studies, we find that generally, when people earn more, they tend to have a better quality of life which can be linked to improved health and ultimately, a higher life expectancy. Also, as generally, men tend to take on more tedious roles in society - heavy lifting and highly demanding blue-collar jobs, it is almost of no surprise that the life expectancy for men is lower than that of women across all income quartiles. +\n"
+
+"Also, it is very worthy to note that analysing data at the States level is not the best as we found, rather harshly that due to the very small amount of data observed at the state level, meaningful insights or trends or seasonality are hard to come by and as such, some measures to be taken when faced with a similar scenario would be getting more data,, preferably at County level for greater observations or merging the data at state level with some other indicators so that at the very least, the dimensionality of exploratory analysis is greater than the initial data received.")
+
 @st.cache  # add caching so we load the data only once
 def load_data(data_file, file_type):
     if file_type == 'csv':
@@ -15,8 +25,9 @@ def load_data(data_file, file_type):
 df1 = load_data("health_ineq_online_table_3.csv", "csv")
 df2 = load_data("health_ineq_online_table_4.csv", "csv")
 df3 = load_data("health_ineq_online_table_5.csv", "csv")
+# df4 = load_data("nst-est2019-alldata.csv", "csv")
 
-st.write("Here's the raw data we would be examining")
+# st.write(df4)
 
 agg_data = df1[["statename", "le_agg_q1_F", "le_agg_q2_F", "le_agg_q3_F", "le_agg_q4_F", "le_agg_q1_M", "le_agg_q2_M", "le_agg_q3_M", "le_agg_q4_M"]]
 agg_data["le_agg_F"] = agg_data[["le_agg_q1_F", "le_agg_q2_F", "le_agg_q3_F", "le_agg_q4_F"]].mean(axis=1)
@@ -34,38 +45,43 @@ for state in states:
     le_timeseries_for_states[state] = state_data
 
 alabama = le_timeseries_for_states['Alabama']
+# alabama_ =pd.DataFrame(columns = ["Male", "Female"])
+# alabama_male = alabama["le_agg"]
 alabama = alabama.melt('year')
+# alabama = alabama.groupby
 
-st.write(alt.Chart(alabama).mark_line().encode(
-    x='year:O',
-    y='value',
-    color='variable:N',
-    # column='site:N'
-)
-)
+# st.write(alt.Chart(alabama).mark_line().encode(
+#     x='year:O',
+#     y='value',
+#     color='variable:N',
+#     # column='site:N'
+# )
+# )
 
 st.write ("Here's our raw data")
 st.write(agg_data)
 st.write("\n")
 st.write(df3)
 
-st.write("Let's see the life expectancy for each state - Male and Female at a glance")
+st.write("Let's see the life expectancy for each state - Male and Female at a glance. Each section is selectable")
 
 # agg_data.index.name = "statename"
 data = agg_data[["statename", "le_agg_q1_F", "le_agg_q2_F", "le_agg_q3_F", "le_agg_q4_F", "le_agg_q1_M", "le_agg_q2_M", "le_agg_q3_M", "le_agg_q4_M"]].melt("statename")
+selector = alt.selection_single(encodings=['x', 'color'])
 chart = alt.Chart(data).mark_bar().encode(
     x=alt.X('statename', title="States"),
     y=alt.Y('sum(value)', title="Life Expectancy at Income Quartiles"),
-    color='variable',
-    tooltip='mean(value)'
+    # color='variable',
+    color=alt.condition(selector, 'variable', alt.value('lightgray')),
+    tooltip=["variable",'mean(value)']
 ).properties(
     width=1000,
     height=400
+).add_selection(
+    selector
 )
 
 st.write(chart)
-
-
 
 st.write ("Next, we see the average life expectancy across all income quartiles by gender")
 
@@ -82,7 +98,7 @@ chart = alt.Chart(agg_data).mark_bar().encode(
     x=alt.X("statename"),
     y=alt.Y("le_agg_M"),
     color="statename",
-    tooltip='statename:N'
+    tooltip=['statename:N', "le_agg_M"]
 ).properties(
     width=1000,
     height=400
@@ -102,31 +118,5 @@ chart2 = alt.Chart(agg_data).mark_bar().encode(
 
 st.write(chart2)
 
-####### Chart 2############
+st.write("Next, we see the distribution of the life expectancies on the map across all states")
 
-highlight = alt.selection(type='single', on='mouseover',
-                          fields=['symbol'], nearest=True)
-
-base = alt.Chart(df3).encode(
-    x='year:T',
-    y='le_agg_q4_F',
-    color='statename'
-)
-
-points = base.mark_circle().encode(
-    opacity=alt.value(0)
-).add_selection(
-    highlight
-).properties(
-    width=600
-).interactive().add_selection(
-    selection
-)
-
-lines = base.mark_line().encode(
-    size=alt.condition(~highlight, alt.value(1), alt.value(3))
-).interactive().add_selection(
-    selection
-)
-
-st.write(points + lines)
--- writeup.md
+++ writeup.md
@@ -1,23 +1,38 @@
-# Project name
+# Social Mobility: Looking at the effect of Income on Life Expectancy 
 
-![A screenshot of your application. Could be a GIF.](screenshot.png)
-
-TODO: Update screenshot
-
-TODO: Short abstract describing the main goals and how you achieved them.
+![A screenshot of your application. Could be a GIF.](visualization.png)
 
 ## Project Goals
 
-TODO: **A clear description of the goals of your project.** Describe the question that you are enabling a user to answer. The question should be compelling and the solution should be focused on helping users achieve their goals. 
+### Describe the question that you are enabling a user to answer. The question should be compelling and the solution should be focused on helping users achieve their goals. 
+Essentially, we set out to find out the relationship between some social indicators and how they affect each other. We looked at life expectancy in the United States states as a consequence of the income distribution. 
+1. We set out to investigate the male life expectancy in all states
+2. We set out to investigate the female life expectancy in all states
+3. We set out to investigate the life expectancy as a time series between 2001 and 2014 across all states in the United States
 
 ## Design
 
-TODO: **A rationale for your design decisions.** How did you choose your particular visual encodings and interaction techniques? What alternatives did you consider and how did you arrive at your ultimate choices?
+### How did you choose your particular visual encodings and interaction techniques? What alternatives did you consider and how did you arrive at your ultimate choices?
+
+We settled on using mostly bar charts for our project and this largely came about after we realised that the data we had contained very little variance and as such any other visualization would not easily convey our findings. We used a combination of stacked bar charts and single column bar charts. The stacked charts portray the different income quartiles for bothe genders and we feel it renders them beautifully.
+
+We also used a map to display a heatmap-like visualization for the life expectancies for all states.
 
 ## Development
 
-TODO: **An overview of your development process.** Describe how the work was split among the team members. Include a commentary on the development process, including answers to the following questions: Roughly how much time did you spend developing your application (in people-hours)? What aspects took the most time?
+### Describe how the work was split among the team members. Include a commentary on the development process, including answers to the following questions: Roughly how much time did you spend developing your application (in people-hours)? What aspects took the most time?
+
+Working together as a team, I believe was easy. The biggest challenge that would have been was communication as one member is in Kigali while the other is in Pittsburgh but we effectively utilised Zoom and emails to synchronise our thoughts and get work done. 
+
+To break down the process: 
+Akintayo Jabar - Created the repository; Led meetings and documentations; Contributed to project code
+Michael Finch - Came up with the agreed research question, conceptualized the manner of approach and provided good feedback; Contributed to project code.
+
+Man-hours spent: 25 hours (Akintayo Jabar); ... (Michael Finch)
 
 ## Success Story
 
-TODO:  **A success story of your project.** Describe an insight or discovery you gain with your application that relates to the goals of your project.
+### Describe an insight or discovery you gain with your application that relates to the goals of your project.
+We came to find that generally, across the United States, the life expectancy is higher for women. We also found that the life expectancy is also highest for the population earning in the 4th percentile of their income bracket. I think this is a very logical trend because as with the typical world bank indicators studies, we find that generally, when people earn more, they tend to have a better quality of life which can be linked to improved health and ultimately, a higher life expectancy. Also, as generally, men tend to take on more tedious roles in society - heavy lifting and highly demanding blue-collar jobs, it is almost of no surprise that the life expectancy for men is lower than that of women across all income quartiles. 
+
+Also, it is very worthy to note that analysing data at the States level is not the best as we found, rather harshly that due to the very small amount of data observed at the state level, meaningful insights or trends or seasonality are hard to come by and as such, some measures to be taken when faced with a similar scenario would be getting more data,, preferably at County level for greater observations or merging the data at state level with some other indicators so that at the very least, the dimensionality of exploratory analysis is greater than the initial data received.
